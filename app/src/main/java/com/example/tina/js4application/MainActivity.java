92f9826716843c638c55c8292750da0f98991fce
Tina <martina.schina@icloud.com>
2018-12-10 12:43:47 +0100

    Inizio controlli email, password

--- app/src/main/java/com/example/tina/js4application/MainActivity.java
+++ app/src/main/java/com/example/tina/js4application/MainActivity.java
@@ -3,18 +3,60 @@ package com.example.tina.js4application;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
 
+    EditText emailET;
+    EditText passwordET;
+
+    Button loginBtn;
+    Button registerBtn;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        emailET = findViewById(R.id.email_et);
+        passwordET = findViewById(R.id.password_et);
+
+        loginBtn = findViewById(R.id.login_btn);
+        registerBtn = findViewById(R.id.register_btn);
+
+        loginBtn.setOnClickListener(this);
+        registerBtn.setOnClickListener(this);
+
         Log.i(TAG,"activity created");
     }
+
+    private boolean isValidEmail(){
+        //TODO how to check id a email is valid??
+        return false;
+    }
+
+    private boolean isValidPassword(){
+        //TODO check the password's lentgh
+        if(passwordET.length() >= 6){
+            return true;
+        }
+        return false;
+    }
+
+    private showErrorMessage(){
+        //TODO show message to the user
+        Log.i(TAG, getString(R.string.login_error));
+    }
+
+    private showSuccesMessage(){
+        //
+        Log.i(TAG, getString(R.string.login_success));
+    }
+
 
     @Override
     protected void onStart() {
@@ -46,5 +88,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "activity destroy");
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.login_btn){
+
+        }
+        else if(view.getId() == R.id.register_btn){
+
+        }
     }
 }
