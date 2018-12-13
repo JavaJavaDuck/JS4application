788c318ad5978e1ecf951216594a3f32d4c3048b
Tina <martina.schina@icloud.com>
2018-12-13 16:06:45 +0100

    Email Click funzionante

--- app/src/main/java/com/example/tina/js4application/WelcomeActivity.java
+++ app/src/main/java/com/example/tina/js4application/WelcomeActivity.java
@@ -1,17 +1,21 @@
 package com.example.tina.js4application;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView welcome;
     TextView userEmail;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
@@ -21,5 +25,16 @@ public class WelcomeActivity extends AppCompatActivity {
         String userMail = getIntent().getStringExtra(MainActivity.welcome);
 
         userEmail.setText(userMail);
+
+        userEmail.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.user_mail) {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto", getIntent().getStringExtra(MainActivity.welcome), null));
+            startActivity(Intent.createChooser(emailIntent, "Send email to..."));
+        }
     }
 }
